34aad85af1bc9f0fe3b87f501c81b2d1f9b6b0d1
Conrad Zelck <git@simpel.cc>
2024-02-25 14:56:43 +0100

    fix index: try manifest.min.json
    Try the minified manifest.min.json. But maybe it had to be the fixed
    filename manifest.json.
    
    Signed-off-by: Conrad Zelck <git@simpel.cc>

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <!-- <title>FreqBuddy</title> -->
     <script src="script.min.js" defer></script>
     <link rel="stylesheet" href="style.min.css">
-    <link rel="manifest" href="/manifest.json">
+    <link rel="manifest" href="/manifest.min.json">
     <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png">
